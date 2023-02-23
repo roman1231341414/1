ac68b0804d4f5cc91beda54e2998d04b0a6cd0c2
roman1231341414 <120581429+roman1231341414@users.noreply.github.com>
2023-02-23 06:05:54 +0300

    2, функция ввода данных

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿/*массив строк из него сделать массив,в котором не больше 3 элементов
 */
 
+string[] M(string message)
+{
+    Console.WriteLine(message);
+    string stroka = Console.ReadLine();
+    var M = stroka.Split(' ');
+    return M;
+}
